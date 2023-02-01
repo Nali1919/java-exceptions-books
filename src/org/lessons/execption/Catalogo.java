e1848396e255c49d7896fb69682895e815dc5744
Nali1919 <qwerty1919979@gmail.com>
2023-02-01 17:18:45 +0100

    Correzione

--- src/org/lessons/execption/Catalogo.java
+++ src/org/lessons/execption/Catalogo.java
@@ -9,41 +9,18 @@ public class Catalogo {
 		System.out.println("Quanti libri?");
 		int numeroLibri = s.nextInt();
 		Libro[] libro = new Libro[numeroLibri];
-		
-		
 
 		for (int i = 0; i < numeroLibri; i++) {
 			System.out.println("Inserisci il nome del libro");
-			String titolo = s.nextLine();
-			s.nextLine();
+			String titolo = s.next();
+			System.out.println("Inserisci autore");
+			String autore = s.next();
+			System.out.println("Inserisci editore");
+			String editore = s.next();
 			System.out.println("Inserisci pagine");
 			int numeroPagine = s.nextInt();
-			s.nextLine();
-			System.out.println("Inserisci autore");
-			String autore = s.nextLine();
-			System.out.println("Inserisci editore");
-			String editore = s.nextLine();
 
-			try {
-				libro[i] = new Libro(titolo, numeroPagine, autore, editore);
-			
-				if (titolo.equals("")) {
-					throw new Exception("non puo' essere vuoto!");
-				}
-				if (numeroPagine <= 0) {
-					throw new Exception("Le pagine non possono essere negative!");
-				}
-				if (autore.equals("")) {
-					throw new Exception("l'autore non puo' essere vuoto!");
-				}
-				if (editore.equals("")) {
-					throw new Exception("l'editore non puo' essere vuoto!");
-				}
-
-			} catch (Exception e) {
-				System.out.println("C'è un errore: " + e.getMessage());
-				i--;
-			}
+			libro[i] = new Libro(titolo, numeroPagine, autore, editore);
 
 		}
 		System.out.println("Ecco i tuoi libri:");
